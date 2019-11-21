fb44ba3dd51f1a1d818a725b5af381bca8af928c
Achim Munene <achim_munene@hotmail.com>
2019-11-21 22:25:28 +0200

    Added Recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(emai='test@closinbrace.com', password='testpass'):
+    """Create a simple user"""
+    return get_user_model().create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -37,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
