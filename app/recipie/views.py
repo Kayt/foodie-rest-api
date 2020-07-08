2d94113d063dfbc05cd4329da9c589cddcb05151
Achim Munene <achim_munene@hotmail.com>
2020-07-08 23:25:26 +0200

    Implemented Tag create feature and tests

--- app/recipie/views.py
+++ app/recipie/views.py
@@ -6,7 +6,9 @@ from core.models import Tag
 from recipie import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permisson_classes = (IsAuthenticated,)
@@ -14,5 +16,11 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
-        """Return objects for the current authenticated ser only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        """Return objects for the current authenticated user only"""
+        if self.request.user.is_authenticated:
+            return self.queryset.filter(user=self.request.user).order_by('-name')
+        return None
+    
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
